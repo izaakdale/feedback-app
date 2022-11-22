6ea85b7b4330a8f99f4012aecf6bb7900209a1a6
izaakdale <64863677+izaakdale@users.noreply.github.com>
2022-11-22 15:04:11 +0000

    Added feedback item with list

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: "Whateverrrrr"
+    },        
+    {
+        id: 2,
+        rating: 7,
+        text: "Whooo"
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: "Damn"
+    },
+]
+
+export default FeedbackData
