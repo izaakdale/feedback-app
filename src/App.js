5e0a12e656ef14e00e511d5e3643082eacb82ded
izaakdale <64863677+izaakdale@users.noreply.github.com>
2022-11-22 16:46:57 +0000

    Added icons for deleting and prop drilling

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
-import Card from "./components/shared/Card"
+import FeedbackStats from "./components/FeedbackStats"
 
 function App() {
 
     const [feedback, setFeedback] = useState(FeedbackData)
 
+    const deleteFeedback = (id) => {
+        if(window.confirm("Are you sure you want to delete?")) {
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+    }
+
     return (
         <div>
             <Header/>
             <div className="container">
-                <FeedbackList feedback={feedback} />
+                <FeedbackStats feedback={feedback}/>
+                <FeedbackList feedback={feedback} handleDelete={(id) => deleteFeedback(id)}/>
             </div>
         </div>
     )
