00cc66afca51af484d7eeeb778fb4699fd050c53
izaakdale <64863677+izaakdale@users.noreply.github.com>
2022-11-23 22:19:21 +0000

    Calculations for average and length

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,7 +1,23 @@
-function FeedbackStats() {
-  return (
-    <div>FeedbackStats</div>
-  )
+import PropTypes from 'prop-types'
+
+function FeedbackStats({feedback}) {
+    // calculate average
+    let average = (feedback.reduce((acc, cur) => {
+        return (acc + cur.rating)
+    }, 0) / feedback.length).toFixed(1)
+
+    console.log(average)
+
+    return (
+        <div className="feedback-stats">
+            <h4>{feedback.length} Reviews</h4>
+            <h4>Average rating: {isNaN(average) ? 0 : average}</h4>
+        </div>
+    )
+}
+
+FeedbackStats.propTypes = {
+    feedback: PropTypes.array.isRequired
 }
 
 export default FeedbackStats
