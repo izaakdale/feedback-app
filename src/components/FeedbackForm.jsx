01d2857e0486a70b2d2725731016ea1c1939a487
izaakdale <64863677+izaakdale@users.noreply.github.com>
2022-11-23 23:00:23 +0000

    Added radio buttons and feedback form box

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useState } from 'react'
+import Card from './shared/Card'
+import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
+
+export default function FeedbackForm() {
+    const [text, setText] = useState('')
+    const [rating, setRating] = useState(10)
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
+
+    const handleTextChange = (input) => {
+        if (text === '') {
+            setBtnDisabled(true)
+            setMessage(null)
+        } else if (text !== 0 && text.trim().length <= 10) {
+            setBtnDisabled(true)
+            setMessage('must be at least 10 chars')
+        } else {
+            setBtnDisabled(false)
+            setMessage(null)
+        }
+
+
+        setText(input.target.value)
+    }
+    
+    return (
+        <Card>
+            <form action="">
+                <h2>How would you rate the service?</h2>
+                <RatingSelect select={setRating} selected={rating}/>
+                <div className='input-group'>
+                    <input onChange={handleTextChange} value={text} type="text" placeholder='write some kind words...'/>
+                    <Button type="submit" isDisabled={btnDisabled}>send</Button>
+                </div>
+                {message && <div className='message'>{message}</div>}
+            </form>
+        </Card>
+    )
+}
